6dba014e847bf63e5fcfadc2203d563699cf4b6a
Devyani Barde <devyanibarde@gmail.com>
2023-01-30 22:02:46 -0500

    Created navbar component

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,15 @@
-import './App.css';
+import NavBar from './NavBar';
 
 function App() {
+  const title = 'Hello'
   return (
     <div className="App">
+      
       <p>
         <h1>Devyani's Portfolio</h1>
+        <NavBar />
+        <br/>
+        <h3>{ title }</h3>
       </p>
     </div>
   );
---
+++ portfolio/src/NavBar.js
@@ -0,0 +1,12 @@
+const NavBar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>Nav Bar</h1>
+            <div className="links">
+                <a href="#">Link</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default NavBar;
