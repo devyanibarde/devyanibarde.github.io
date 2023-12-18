d365d5f3597ec1de9f226430c58a8ec1281da5cf
Devyani Barde <devyanibarde@gmail.com>
2023-12-18 13:48:39 -0500

    Revert "Changes"
    This reverts commit 23e2933bd4099a7d76aa52b12e37691ea5d18849.

--- portfolio/src/Projects.js
+++ portfolio/src/Projects.js
@@ -2,48 +2,16 @@ import { Carousel } from 'react-responsive-carousel';
 import styles from 'react-responsive-carousel/lib/styles/carousel.min.css';
 const Projects = () => {
     let proj = [
-        {
-            pname: 'Facial Emotion Recognition', 
-            gitlink: 'https://github.com/devyanibarde/FacialEmotionRecognition', 
-            desc: 'A computer visison based machine learning model is developed to detect seven different emotions in any given facial image.', 
-            id: 1
-        },
-        {
-            pname: 'Market', 
-            gitlink: 'https://github.com/devyanibarde/Market', 
-            desc: 'A web portal for online purchases, created with Django framework.', 
-            id: 2
-        },
-        {
-            pname: 'Plant Pathology', 
-            gitlink: 'https://github.com/devyanibarde/PlantPathology', 
-            desc: 'Machine learning-based model to accurately classify a given leaf image from the test dataset to a particular disease category, and to identify an individual disease from multiple disease symptoms on a single leaf image.', 
-            id: 3
-        },
-        {
-            pname: 'Identification of Meritorious Students', 
-            gitlink: 'https://github.com/devyanibarde/IoMS', 
-            desc: 'A web portal for schools to manage student data.', 
-            id: 4
-        }
+        {pname: 'Facial Emotion Recognition', gitlink: 'https://github.com/devyanibarde/FacialEmotionRecognition', desc: 'A computer visison based machine learning model is developed to detect seven different emotions in any given facial image.', id: 1},
+        {pname: 'Market', gitlink: 'https://github.com/devyanibarde/Market', desc: 'A web portal for online purchases, created with Django framework.', id: 2},
+        {pname: 'Plant Pathology', gitlink: 'https://github.com/devyanibarde/PlantPathology', desc: 'Machine learning-based model to accurately classify a given leaf image from the test dataset to a particular disease category, and to identify an individual disease from multiple disease symptoms on a single leaf image.', id: 3},
+        {pname: 'Identification of Meritorious Students', gitlink: 'https://github.com/devyanibarde/IoMS', desc: 'A web portal for schools to manage student data.', id: 4}
       ];
     let tech = [
-        {
-            data: ['Python', 'Keras', 'Jupyter', 'Matplotlib'], 
-            s_id: 1
-        },
-        {
-            data: ['Python', 'Django', 'HTML', 'CSS'], 
-            s_id: 2
-        },
-        {
-            data: ['Python', 'Tensorflow', 'Jupyter'], 
-            s_id: 3
-        },
-        {
-            data: ['Java', 'HTML', 'CSS', 'MySQL'], 
-            s_id: 4
-        }
+        {data: ['Python', 'Keras', 'Jupyter', 'Matplotlib'], s_id: 1},
+        {data: ['Python', 'Django', 'HTML', 'CSS'], s_id: 2},
+        {data: ['Python', 'Tensorflow', 'Jupyter'], s_id: 3},
+        {data: ['Java', 'HTML', 'CSS', 'MySQL'], s_id: 4}
       ];
     return ( 
         <div className="projects" id="projects">
@@ -51,6 +19,7 @@ const Projects = () => {
             <h1>My Projects:</h1>
             <Carousel autoPlay="True" interval={6000} centerMode="True" infiniteLoop="True">
             {proj.map((proj) => (
+                
                 <div className="projdiv" key={proj.id}>
                     <table>
                         <tr>
@@ -77,6 +46,7 @@ const Projects = () => {
                         </div>
                     ))}
                 </div>
+                
             ))}
             </Carousel>
         </div>
